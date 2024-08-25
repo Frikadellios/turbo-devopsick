853f235606f58352c157747ec3ead2e497cc1a6b
Frikadellios <devopsick@pm.me>
2024-08-26 01:36:28 +0300

    add front with astro && tw4

---
+++ apps/web/astro.config.ts
@@ -0,0 +1,14 @@
+import mdx from '@astrojs/mdx'
+import partytown from '@astrojs/partytown'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	integrations: [react(), sitemap(), partytown(), mdx()]
+})
